40ec6f34841719da9b450d91bd7261b70841bf71
Pranav K <prkrishn@hotmail.com>
2019-04-08 14:07:05 -0700

    Require Auth

--- Startup.cs
+++ Startup.cs
@@ -16,6 +16,7 @@ namespace LocalBenchmarks
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthorization();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
