f5dbd6c8444bf843267bbd05ea3d5b07696a4ddb
Pranav K <prkrishn@hotmail.com>
2019-04-08 14:08:24 -0700

    Require Auth

--- Startup.cs
+++ Startup.cs
@@ -16,6 +16,7 @@ namespace LocalBenchmarks
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication();
             services.AddAuthorization();
         }
 
